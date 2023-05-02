691081c099e66da30d8977dca6e81a07cb61973d
Vivian <bergerv.tin22@student.dhbw-heidenheim.de>
2023-05-02 17:13:34 +0200

    Adjusted style on page.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,37 @@
 use yew::prelude::*;
+use gloo_timers;
+use gloo_timers::callback::Timeout;
 
 #[function_component(App)]
 fn app() -> Html {
-    let counter = use_state(|| 0);
-         // let onclick = {
-         // let counter = counter.clone();
-         //    move |_| {
-         //        let value = *counter + 1;
-         //        counter.set(value);
-         //    }
-         // };
+    let watering_icon = use_state(|| "wateringcan.png" );
+    let watering_action = {
+        let watering_icon = Clone::clone(&watering_icon);
+        move |_: MouseEvent| {
+            let value = "wateringcan3.gif";
+            watering_icon.set(value);
+
+            {
+                let watering_icon = Clone::clone(&watering_icon.clone());
+                Timeout::new(3000, move || watering_icon.set("wateringcan.png"))
+                    .forget();
+            }
+        }
+    };
 
     html! {
-            <div class="container is-fluid">
+            <div class="container hero is-fluid is-fullheight">
+
                 <div class="title is-1">
                     <h1 id={ "projectName" }>
-                        <span class={ "blackName" }>{ "t" }</span>
-                        <span class={ "redName" }>{ "rust" }</span>
-                        <span class={ "blackName" }>{ "y gardener" }</span>
+                        <span class={ "blackName" }>{ "T" }</span>
+                        <span class={ "redName" }>{ "RUST" }</span>
+                        <span class={ "blackName" }>{ "Y GARDENER" }</span>
                     </h1>
                 </div>
                 <div class="columns">
                     <div class="column is-two-thirds">
-                        <div class="subtitle is-2">{"Beschreibung:"}</div>
+                        <div class="subtitle is-3">{"Beschreibung:"}</div>
                         <div class="tile is-ancestor">
                             <div class="tile is-parent">
                                 <article class="tile is-child box">
@@ -37,6 +46,8 @@ fn app() -> Html {
                             <div class="tile is-parent">
                                 <article class="tile is-child box">
                                     <p class="title">{"Gießen"}</p>
+                                    <p class="subtitle is-4">{ "Drücke auf das Bild nach dem Gießen:" }</p>
+                                    <img onclick={watering_action} src={*watering_icon} title="watering can" width="100" height="100"/>
                                 </article>
                             </div>
                         </div>
@@ -56,21 +67,22 @@ fn app() -> Html {
                     <div class="column">
                         <div class="tile is-ancestor">
                             <div class="tile is-parent">
-                                <div class="tile is-child box">
+                                <article class="tile is-child box">
                                     <img src="plant1.png" alt="a plant" />
-                                </div>
+                                </article>
                             </div>
                         </div>
                         <div class="tile is-ancestor">
                             <div class="tile is-parent">
-                                <div class="tile is-child box">
+                                <article class="tile is-child box">
                                     <p class="title">{ "submit your plant" }</p>
-                                </div>
+                                </article>
                             </div>
                         </div>
                     </div>
                 </div>
-                <footer class="footer">
+
+                <footer class="footer mt-auto">
                     <div class="content has-text-centered">
                         <p>{"footer"}</p>
                     </div>
